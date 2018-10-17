266d615d99a2149c327efcbc0c057af409b0c0ea
Huy <huyct3105@gmail.com>
2018-10-17 15:07:45 +0700

    Create project

---
+++ app/src/main/java/minhnq/gvn/com/openweathermap/MainActivity.java
@@ -0,0 +1,13 @@
+package minhnq.gvn.com.openweathermap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
